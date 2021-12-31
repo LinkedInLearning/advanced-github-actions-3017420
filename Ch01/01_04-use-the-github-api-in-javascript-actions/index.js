94d1cd39ac82dee30a904211a72b77e7ebf84190
Michael Jenkins <managedkaos@users.noreply.github.com>
2021-12-31 00:24:21 -0800

    Initial files for 01-04

---
+++ Ch01/01_04-use-the-github-api-in-javascript-actions/index.js
@@ -0,0 +1,11 @@
+// require the libraries for actions
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+// use an async function for the main tasks
+async function main() {
+    console.log('Hello, Universe!')
+}
+
+// call the function
+main();
